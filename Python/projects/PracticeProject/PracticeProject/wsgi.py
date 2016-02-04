22e597e60dd14996e5162bd944b55a9de4a72c75
shikha-desai <shikha.desai@ishisystems.com>
2016-02-04 16:29:56 +0530

    Created PracticeProject in pycharm with app LoginApp

--- Python/projects/PracticeProject/PracticeProject/wsgi.py
+++ Python/projects/PracticeProject/PracticeProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projects project.
+WSGI config for PracticeProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projects.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PracticeProject.settings")
 
 application = get_wsgi_application()
