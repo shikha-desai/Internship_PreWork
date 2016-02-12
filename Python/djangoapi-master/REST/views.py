b7d9b4bf5238882ce86b3b5f7b94d2520b1c5070
shikha-desai <shikha.desai@ishisystems.com>
2016-02-12 12:26:29 +0530

    Created streaming view in django api using file chunks

--- Python/djangoapi-master/REST/views.py
+++ Python/djangoapi-master/REST/views.py
@@ -63,6 +63,42 @@ def GetMedia(request, path):
         logger.info(fpathnew)
 
         with open(fpathnew,'wb') as new:
+            new.seek(0)
+            new.truncate()
             copyfile(fpath, fpathnew)
 
     return HttpResponseRedirect('http://127.0.0.1:8000/')
+
+# def GetMedia(request, path):
+#     logger = logging.getLogger(__name__)
+#     logger.info(path)
+#     fpath = os.path.abspath(os.path.join(settings.MEDIA_ROOT,path))
+#     logger.info(fpath)
+#     with open(fpath,'rb') as f:
+#         logger.info('file paths:')
+#         name = f.name.split('/')
+#         name.reverse()
+#         fname = os.path.join(name[1], name[0])
+#         logger.info(fname)
+#         fpathnew = os.path.join('/home/shikha.desai/DownloadTest/media/',fname)
+#         logger.info(fpathnew)
+#         logger.info("Size of old file")
+#         size = os.path.getsize(fpath)
+#         logger.info(size)
+#         with open(fpathnew,'ab') as new:
+#             new.seek(0)
+#             new.truncate()
+#             i = 0
+#             chunk_size = 1024
+#             while True:
+#                 chunk = f.read(chunk_size)
+#                 if f.tell() == size:
+#                     break
+#                 new.write(bytes(chunk))
+#                 i += 1
+#                 s = "Writing chunk {}".format(i)
+#                 logger.info(s)
+#             logger.info("Size of new file")
+#             logger.info(os.path.getsize(fpathnew))
+#
+#     return HttpResponseRedirect('http://127.0.0.1:8000/')
